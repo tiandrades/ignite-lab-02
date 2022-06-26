01da19322bbce42520a0d0a82249cf350e5e58af
Tiago Andrade <tailergamer7@gmail.com>
2022-06-25 23:43:43 -0400

    fix: Remake image import

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,9 +1,11 @@
-import { gql, useMutation } from '@apollo/client';
 import { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Logo } from '../components/Logo';
 import { useCreateSubscriberMutation } from '../graphql/generated';
+
+import { Logo } from '../components/Logo';
+
+import CodeMockupImage from '../assets/code-mockup.png'; 
 
 export function Subscribe() {
   const navigate = useNavigate();
@@ -68,7 +70,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="Imagem de código" />
+      <img src={CodeMockupImage} className="mt-10" alt="Imagem de código" />
     </div>
   );
 }
